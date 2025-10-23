f8f18a51e9a1f2f2f0f0f8ab478e77a3ec458c8e
Claude BIVER <claude.biver@pm.me>
2025-10-23 12:44:38 +0200

    fix: remove additional locale settings

--- configuration.nix
+++ configuration.nix
@@ -42,23 +42,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "de_DE.UTF-8/UTF-8"
-  ];
-
-  i18n.extraLocaleSettings = {
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LANG = "en_US.UTF-8";
-  };
+ 
 
   # Add Japanese input
   i18n.inputMethod = {
