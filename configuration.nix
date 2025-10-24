afc911316d52ffa3cb6cee105e6090090ca2bb2b
Claude BIVER <claude.biver@pm.me>
2025-10-24 16:28:11 +0200

    feat: add modified lutris

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,13 @@
     pciutils
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget  
+    # Lutris
+    (pkgs.unstable.lutris.override {
+      extraPkgs = pkgs: [
+        pkgs.wineWowPackages.stagingFull
+        pkgs.winetricks
+      ];
+    })
   ];
 
   programs.vim.defaultEditor = true;
