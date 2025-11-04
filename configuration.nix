e6e2ab6de25d3a4db4cc33df3b0d640cf1269ffb
Claude BIVER <claude.biver@pm.me>
2025-11-04 15:17:47 +0100

    fix: remove virtualisation

--- configuration.nix
+++ configuration.nix
@@ -177,7 +177,7 @@
   ];
 
   # Enable virtualisation
-  virtualisation.vmware.host.enable = true; 
+  # virtualisation.vmware.host.enable = true; 
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
