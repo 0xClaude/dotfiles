6af7db9233c465fb3e4d4174383693949e959604
Claude BIVER <claude.biver@pm.me>
2025-11-16 15:01:45 +0100

    chore: re-organise some apps/nix files

--- modules/system.nix
+++ modules/system.nix
@@ -3,10 +3,15 @@
 { 
   home.packages = with pkgs; [
     bat
+    fzf
+    imagemagick
+    mpv
     nix-search
     ncdu
     nh
     oh-my-zsh
+    qalculate-qt
+    tig
     timeshift
     zsh-autosuggestions
   ];
