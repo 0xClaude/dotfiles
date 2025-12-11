2d672e7652ca50293955fd8e998331dcfe2034d7
Claude BIVER <claude.biver@pm.me>
2025-12-11 06:26:21 +0100

    fix: remove beaver notes

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-25.05";
 
-		# Add the unstable channel as a second nixpkgs
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
 		home-manager = {
@@ -17,15 +16,9 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		# Beaver-notes
-		beaver-notes-src = {
-			url = "github:Beaver-Notes/Beaver-Notes";
-			flake = false;
-		};
-
 	};
 
-	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, silentSDDM, beaver-notes-src, ... }: {
+	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, silentSDDM, ... }: {
 
 		nixosConfigurations.nebula = nixpkgs.lib.nixosSystem {
 
@@ -46,13 +39,6 @@
 				home-manager.nixosModules.home-manager
 				{
 					nixpkgs.config.allowUnfree = true;
-
-					nixpkgs.overlays = [
-						(import ./overlays/beaver-notes.nix {
-							pkgs = import nixpkgs { system = "x86_64-linux"; };
-							beaver-src = beaver-notes-src;
-						})
-					];
 
 					home-manager = {
 						useGlobalPkgs = true;
