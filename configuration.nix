e63600a200fd64f954083b40cd433d97b2dba1a6
Claude BIVER <claude.biver@pm.me>
2025-12-18 10:46:30 +0100

    chore: correct indenteation

--- configuration.nix
+++ configuration.nix
@@ -109,23 +109,23 @@
     };
 
     settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [ "nix-command" "flakes" ];
 
       builders-use-substitutes = true;
 
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://niri.cachix.org"
-    ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://niri.cachix.org"
+      ];
 
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-    ];
+      ];
 
-    trusted-users = [ "root" "claude" ];
+      trusted-users = [ "root" "claude" ];
     }
   };
 
